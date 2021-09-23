46bec39e4c9c682aa2a61725db343381ce8c62c3
Bato Balint <balint.bato@gmail.com>
2021-09-23 19:24:44 +0200

    ui fejlesztve

--- app/src/main/java/com/example/colorpicker/MainActivity.kt
+++ app/src/main/java/com/example/colorpicker/MainActivity.kt
@@ -14,10 +14,57 @@ class MainActivity : AppCompatActivity() {
     lateinit var ColorLayout : RelativeLayout
     lateinit var TextView : TextView
 
+    var red = 0
+    var green = 0
+    var blue = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Init()
+        RedSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+
+        })
+
+        GreenSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+
+        })
+
+        BlueSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+
+        })
+
     }
 
     fun Init() {
